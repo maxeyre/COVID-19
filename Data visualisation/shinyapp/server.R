418d42ff2b79c1a2e3f2c7a20c4512aaba0f0705
Max <maxeyre3@gmail.com>
2020-03-20 14:12:48 +0000

    all

--- Data visualisation/shinyapp/server.R
+++ Data visualisation/shinyapp/server.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(readr)
 library(tidyverse)
 library(grid)
+library(ggplot2)
+library(gridExtra)
+library(gtable)
 
 # read in global data
 data <- read_csv("https://raw.githubusercontent.com/andrewlilley/tool_COVID-19/master/output_data/country_level.csv?token=ANJMHSDBYB4LQYORA77LET26PTC74")
@@ -17,44 +20,32 @@ data$date = as.Date(data$date, "%Y-%m-%d")
 
 data <- data[order(data$country),]
 
-# calculate new daily cases
+# calculate new daily cases, deaths, recoveries
 data$new_cases <- c()
-uni.country <- unique(data$country)
-out <- c()
-for (i in 1:length(uni.country)){
-  x <- data[data$country==uni.country[i],]
-  out <- c(out,0,diff(x$total_cases))
-}
-
-data$new_cases <- out
-
-# calculate new daily deaths
 data$new_deaths <- c()
-out <- c()
-for (i in 1:length(uni.country)){
-  x <- data[data$country==uni.country[i],]
-  out <- c(out,0,diff(x$total_deaths))
-}
-
-data$new_deaths <- out
-
-# calculate new daily recoveries
 data$new_recovered <- c()
-out <- c()
+uni.country <- unique(data$country)
+out.cases <- c()
+out.deaths <- c()
+out.recovered <- c()
 for (i in 1:length(uni.country)){
   x <- data[data$country==uni.country[i],]
-  out <- c(out,0,diff(x$total_recovered))
+  out.cases <- c(out.cases,0,diff(x$total_cases))
+  out.deaths <- c(out.deaths,0,diff(x$total_deaths))
+  out.recovered <- c(out.recovered,0,diff(x$total_recovered))
 }
 
-data$new_recovered <- out
+data$new_cases <- out.cases
+data$new_deaths <- out.deaths
+data$new_recovered <- out.recovered
 
 data <- gather(data, key="type", value="number",3:ncol(data))
 
 # UK county data
 # read in UK county data
-data.county <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTNL88hteOvWd8ZafF_h0-38Mg1Z3PJdcBkNI3fvORLbuqIlWzc3btPPmD1sB_xqL6jCHcgU1i50ukX/pub?gid=0&single=true&output=csv"
+data.county <- "https://raw.githubusercontent.com/maxeyre/COVID-19/master/Data%20visualisation/UK%20data/england_countyUA.csv"
 data.county <- read_csv(data.county)
-data.county <- gather(data.county, key="date", value="cases",3:ncol(data.county))
+data.county <- gather(data.county, key="date", value="total_cases",3:ncol(data.county))
 data.county$date = as.Date(data.county$date, "%d/%m/%Y")
 
 data.county <- data.county[order(data.county$county_UA),]
@@ -65,9 +56,11 @@ uni.county <- unique(data.county$county_UA)
 out <- c()
 for (i in 1:length(uni.county)){
   x <- data.county[data.county$county_UA==uni.county[i],]
-  out <- c(out,0,diff(x$cases))
+  out <- c(out,0,diff(x$total_cases))
 }
 data.county$new_cases <- out
+#data.county$new_cases[data.county$new_cases<0]<- 0
+data.county <- gather(data.county,key="type",value="number",4:ncol(data.county))
 
 # get list of counties
 data.county$county_UA <- as.character(data.county$county_UA)
@@ -77,6 +70,42 @@ for (i in 1:length(county_LA.list)){
   list.county[i] <- county_LA.list[i]
 }
 names(list.county) <- county_LA.list
+
+# read in England region data
+data.region <- read_csv("https://raw.githubusercontent.com/maxeyre/COVID-19/master/Data%20visualisation/UK%20data/england_region.csv")
+data.region <- gather(data.region, key="date", value="cases",3:ncol(data.region))
+data.region$date = as.Date(data.region$date, "%d/%m/%Y")
+data.region <- data.region %>%
+  rename(region = NHSRNm, total_cases =cases)
+data.region <- data.region[order(data.region$region),]
+# get list of regions
+data.region$region <- as.character(data.region$region)
+region.list <- c(unique(data.region$region))
+list.region <- list()
+for (i in 1:length(region.list)){
+  list.region[i] <- region.list[i]
+}
+names(list.region) <- region.list
+# calculate new daily cases
+data.region$new_case <- c()
+uni.region <- unique(data.region$region)
+out <- c()
+for (i in 1:length(uni.region)){
+  x <- data.region[data.region$region==uni.region[i],]
+  out <- c(out,0,diff(x$total_cases))
+}
+data.region$new_cases <- out
+data.region <- gather(data.region,key="type",value="number",4:ncol(data.region))
+
+# Testing data
+data.test <- read_csv("https://raw.githubusercontent.com/maxeyre/COVID-19/master/Data%20visualisation/UK%20data/UK_testing.csv")
+data.test <- data.test %>%
+  select(date, total_tested = tested)
+data.test$date = as.Date(data.test$date, "%d/%m/%Y")
+data.test$new_tested <- c(NA,diff(data.test$total_tested))
+data.test <- gather(data.test, key="type", value="number",2:ncol(data.test))
+
+# UK data
 
 
 # Define server logic required to plot various variables against mpg
@@ -111,10 +140,31 @@ shinyServer(function(input, output) {
     tagList(url)
     })
   
-  url_data <- a("Data source", href="")
+  output$twitter2 <- renderUI({
+    tagList(url)
+  })
+  output$twitter3 <- renderUI({
+    tagList(url)
+  })
+  output$twitter4 <- renderUI({
+    tagList(url)
+  })
+  
+  url_data <- a("Data sources", href="https://github.com/CSSEGISandData/COVID-19")
+  url_data2 <- a("Data source", href="https://www.gov.uk/guidance/coronavirus-covid-19-information-for-the-public")
   
   output$data_source <- renderUI({
     tagList(url_data)
+  })
+  
+  output$data_source2 <- renderUI({
+    tagList(url_data2)
+  })
+  output$data_source3 <- renderUI({
+    tagList(url_data2)
+  })
+  output$data_source4 <- renderUI({
+    tagList(url_data2)
   })
   
   output$checkGroup <- renderText({
@@ -122,14 +172,17 @@ shinyServer(function(input, output) {
   })
   
   output$checkGroup_county <- renderText({
-    paste(as.character(length(input$checkGroup_county)))
+    paste(as.character(c(input$checkGroup_county)))
+  })
+  output$checkGroup_region <- renderText({
+    paste(as.character(c(input$checkGroup_region)))
   })
     
   # Single country plots
   output$countryPlot <- renderPlot({
     lines <- c(as.character(input$checkGroup))
     
-    data <- data[data$type %in% lines, ]
+    data<- data[data$type %in% lines, ]
     
       p <- ggplot(data[data$country==paste(formulaText(),sep=""),]) + geom_point(aes(x=date, y=number, col=type),size=1.5) +
         geom_line(aes(x=date, y=number, col=type),size=1) +
@@ -139,12 +192,12 @@ shinyServer(function(input, output) {
               axis.title=element_text(size=16), 
               axis.title.x = element_text(vjust=-1.5),
               axis.title.y = element_text(vjust=2),
-              legend.text = element_text(size=16),
+              legend.text = element_text(size=13),
               legend.position = 'top', 
               legend.spacing.x = unit(0.4, 'cm'),
               panel.grid.major.y=element_line(size=0.05)) +
         scale_colour_manual(name="",values = c("total_cases" = "#000000", "new_cases" = "#e41a1c", "total_deaths"="#ff7f00", 
-                                       "new_deaths"="#a65628","total_recovered"="#377eb8", "new_recovered"="#4daf4a"),
+                                               "new_deaths"="#a65628","total_recovered"="#377eb8", "new_recovered"="#4daf4a"),
                             breaks=c("new_cases","total_cases","new_deaths","total_deaths","new_recovered","total_recovered"),
                             labels=c("Cases (daily)", "Cases (total)", "Deaths (daily)","Deaths (total)","Recovered (daily)","Recovered (total)")) +
         guides(linetype = guide_legend(override.aes = list(size = 20)))
@@ -154,29 +207,82 @@ shinyServer(function(input, output) {
       p
   })
 
-  # UK country plots
-  output$UKcountyPlot <- renderPlot({
-    lines <- c(as.character(input$checkGroup_county))
+  # England NHS regions plots
+  output$EnglandRegionPlot <- renderPlot({
     
-    data.county <- data.county[data.county$type %in% lines, ]
+    lines <- c(as.character(input$checkGroup_region))
     
-    ggplot(data.county[data.county$country==paste(formulaText_county(),sep=""),]) + geom_point(aes(x=date, y=number, col=type),size=1.5) +
-      geom_line(aes(x=date, y=number, col=type),size=1) +
-      scale_x_date(limits=c(input$dateRange[1],input$dateRange[2])) + xlab(label = "") +ylab(label="Cases") +
+    data.region<- data.region[data.region$type %in% lines, ]
+  
+    ggplot(data.region) + geom_point(aes(x=date, y=number, col=region),size=1.5) +
+      geom_line(aes(x=date, y=number, col=region, linetype=type),size=1) +
+      scale_x_date(limits=c(input$dateRange_region[1],input$dateRange_region[2])) + xlab(label = "") +ylab(label="Cases") +
       theme_classic()+
       theme(axis.text=element_text(size=13),
             axis.title=element_text(size=16), 
             axis.title.x = element_text(vjust=-1.5),
             axis.title.y = element_text(vjust=2),
-            legend.text = element_text(size=16),
+            legend.title = element_blank(),
+            legend.text = element_text(size=13),
             legend.position = 'top', 
             legend.spacing.x = unit(0.4, 'cm'),
             panel.grid.major.y=element_line(size=0.05)) +
-      scale_colour_manual(name="",values = c("total_cases" = "#000000", "new_cases" = "#e41a1c", "total_deaths"="#ff7f00", 
-                                             "new_deaths"="#a65628"),
-                          breaks=c("new_cases","total_cases","new_deaths","total_deaths"),
-                          labels=c("Cases (daily)", "Cases (total)", "Deaths (daily)","Deaths (total)")) +
+      scale_linetype_manual(name="", values=c("total_cases"=1, "new_cases" = 2),
+                            breaks=c("total_cases","new_cases"),
+                            labels=c("Cases (total)","Cases (daily)")) +
+      guides(linetype = guide_legend(label.position = "top", keywidth = 2))
+  })
+  
+  # England county plots
+  output$englandcountyPlot <- renderPlot({
+    lines <- c(as.character(input$checkGroup_county))
+    
+    data.county <- data.county[data.county$type %in% lines, ]
+    
+    ggplot(data.county[data.county$county_UA==paste(formulaText_county(),sep=""),]) + geom_point(aes(x=date, y=number, col=type),size=1.5) +
+      geom_line(aes(x=date, y=number, col=type),size=1) +
+      scale_x_date(limits=c(input$dateRange_county[1],input$dateRange_county[2])) + xlab(label = "") +ylab(label="Cases") +
+      theme_classic()+
+      theme(axis.text=element_text(size=13),
+            axis.title=element_text(size=16), 
+            axis.title.x = element_text(vjust=-1.5),
+            axis.title.y = element_text(vjust=2),
+            legend.text = element_text(size=13),
+            legend.position = 'top', 
+            legend.spacing.x = unit(0.4, 'cm'),
+            panel.grid.major.y=element_line(size=0.05)) +
+      scale_colour_manual(name="",values = c("total_cases" = "#000000", "new_cases" = "#e41a1c"),
+                          breaks=c("new_cases","total_cases"),
+                          labels=c("Cases (daily)", "Cases (total)")) +
       guides(linetype = guide_legend(override.aes = list(size = 20)))
+  })
+  
+  # UK testing plot
+  output$UKtestingPlot <- renderPlot({
+    
+    lines <- c(as.character(input$checkGroup_test))
+    
+    data.test <- data.test[data.test$type %in% lines, ]
+    
+    p.test <- ggplot(data.test) + geom_point(aes(x=date, y=number, col=type),size=1.5)+ 
+      geom_line(aes(x=date, y=number, col=type, group=type),size=1) +
+      scale_x_date(limits=c(input$dateRange_test[1],input$dateRange_test[2])) + xlab(label = "") +ylab(label="Number tested") +
+      theme_classic()+
+      theme(axis.text=element_text(size=13),
+            axis.title=element_text(size=16), 
+            axis.title.x = element_text(vjust=-1.5),
+            axis.title.y = element_text(vjust=2),
+            legend.text = element_text(size=13),
+            legend.position = 'top', 
+            legend.spacing.x = unit(0.4, 'cm'),
+            panel.grid.major.y=element_line(size=0.05)) +
+      scale_colour_manual(name="",values = c("total_tested" = "#000000", "new_tested" = "#e41a1c"),
+                          breaks=c("new_tested","total_tested"),
+                          labels=c("Daily", "Total"))
+    if(input$log_test=='log_yes'){
+      p.test <- p.test + scale_y_continuous(trans='log10')
+    }
+    p.test
   })
   
   
--- Data visualisation/shinyapp/ui.R
+++ Data visualisation/shinyapp/ui.R
@@ -4,16 +4,6 @@ library(shiny)
 library(dplyr)
 library(tidyverse)
 
-# read in global data (from World in Data) - no longer using
-# data <- "http://cowid.netlify.com/data/full_data.csv"
-# data <- read_csv(data)
-# data[is.na(data)] <- 0
-# 
-# data$date = as.Date(data$date, "%Y-%m-%d")
-# 
-# data <- data %>%
-#   rename(country=location)
-
 # read in Andrew Lilley data
 data <- read_csv("https://raw.githubusercontent.com/andrewlilley/tool_COVID-19/master/output_data/country_level.csv?token=ANJMHSDBYB4LQYORA77LET26PTC74")
 
@@ -26,13 +16,8 @@ data <- data[order(data$country),]
 
 # list of countries
 data$country <- as.character(data$country)
-
 country.list <- c(unique(data$country))
-#country.list <- c(country.list[(1:(which(country.list=="Others"))-1)],country.list[(which(country.list=="Others")+1):length(country.list)])
-
-
 list.50 <- list()
-
 for (i in 1:length(country.list)){
   list.50[i] <- country.list[i]
 }
@@ -44,7 +29,7 @@ data.county <- read_csv(data.county)
 data.county <- gather(data.county, key="date", value="cases",3:ncol(data.county))
 data.county$date = as.Date(data.county$date, "%d/%m/%Y")
 
-# get list of countries with more than 50 cases
+# get list of counties
 data.county$county_UA <- as.character(data.county$county_UA)
 county_LA.list <- c(unique(data.county$county_UA))
 list.county <- list()
@@ -53,6 +38,29 @@ for (i in 1:length(county_LA.list)){
 }
 names(list.county) <- county_LA.list
 
+# read in England region data
+data.region <- read_csv("https://raw.githubusercontent.com/maxeyre/COVID-19/master/Data%20visualisation/UK%20data/england_region.csv")
+data.region <- gather(data.region, key="date", value="cases",3:ncol(data.region))
+data.region$date = as.Date(data.region$date, "%d/%m/%Y")
+data.region <- data.region %>%
+  rename(region = NHSRNm)
+# get list of regions
+data.region$region <- as.character(data.region$region)
+region.list <- c(unique(data.region$region))
+list.region <- list()
+for (i in 1:length(region.list)){
+  list.region[i] <- region.list[i]
+}
+names(list.region) <- region.list
+
+# Testing data
+data.test <- read_csv("https://raw.githubusercontent.com/maxeyre/COVID-19/master/Data%20visualisation/UK%20data/UK_testing.csv")
+data.test <- data.test %>%
+  select(date, total_tested = tested)
+data.test$date = as.Date(data.test$date, "%d/%m/%Y")
+data.test$new_tested <- c(NA,diff(data.test$total_tested))
+data.test <- gather(data.test, key="type", value="number",2:ncol(data.test))
+
 # Define UI 
 shinyUI(
   navbarPage("Navbar",
@@ -60,8 +68,8 @@ shinyUI(
   # Sidebar with controls to select the variable to plot against mpg
   # and to specify whether outliers should be included
     tabPanel("By country",
-             headerPanel("COVID-19 Epidemic Curves"),   
-             h3("Live epidemic curves"),
+             headerPanel("COVID-19 Data Visualisation"),   
+             h3("Live epidemic curves by country"),
               h6("Data source: Collected directly from JHU CSSE sources by Andrew Lilley (updated every 24hrs)"),
               h6("Please note: Confirmed case data is entirely dependent on testing rates and will significantly underestimate actual number of infected individuals"),
                 sidebarLayout(
@@ -87,24 +95,53 @@ shinyUI(
                 mainPanel( # don't want this in the side bar!
                   h3(textOutput("caption")),
                   textOutput("startdate"),
-                  plotOutput("countryPlot")
+                  plotOutput("countryPlot"),
+                  h6("Made by Max Eyre"),
+                  h6("Any comments, questions or suggestions please contact via twitter or max.eyre@lstmed.ac.uk"),
+                  uiOutput("twitter"),
+                  uiOutput("data_source")
                   )
                 )
              
     ),
-             
-    #tabPanel("Country comparisons"),
-    tabPanel("UK counties",
-           headerPanel("COVID-19 Epidemic Curves"),   
+  tabPanel("NHS England regions",
+           headerPanel("COVID-19 Data Visualisation"),   
+           h3("Live epidemic curves by NHS England regions"),
+           h6("Data source: Public Health England (updated every 24hrs)"),
+           h6("Please note: Confirmed cases in the UK are now generally individuals presenting at hospitals"),
+           sidebarLayout(
+             sidebarPanel(
+               checkboxGroupInput("checkGroup_region", "", choices = list("Cases (daily)" = "new_cases", 
+                                                                          "Cases (total)" = "total_cases"),selected = 2),
+               dateRangeInput("dateRange_region", "Date range",
+                              start  = min(data.region$date),
+                              end    = max(data.region$date), 
+                              min    = min(data.region$date),
+                              max    = max(data.region$date))
+             ),
+             mainPanel(
+               plotOutput("EnglandRegionPlot"),
+               h6("Made by Max Eyre"),
+               h6("Any comments, questions or suggestions please contact via twitter or max.eyre@lstmed.ac.uk"),
+               uiOutput("twitter2"),
+               uiOutput("data_source2")
+             )
+           )
+  ),
+           
+    tabPanel("England counties",
+           headerPanel("COVID-19 Data Visualisation"),   
            h3("Live epidemic curves for counties of England (Unitary Areas)"),
            h6("Data source: Public Health England (updated every 24hrs)"),
-           h6("Please note: Confirmed case data is entirely dependent on testing rates and will significantly underestimate actual number of infected individuals"),
+           h6("Please note:"),
+           h6("1. PHE data sometimes lose cases between days (hence negative new cases), hopefully this will improve with time."),
+           h6("2. Confirmed case data is entirely dependent on testing rates and will significantly underestimate actual number of infected individuals"),
             sidebarLayout(
              sidebarPanel(
                selectInput("county", "County (UA):",list.county),
                checkboxGroupInput("checkGroup_county", "", choices = list("Cases (daily)" = "new_cases", 
                                                                    "Cases (total)" = "total_cases"),selected = 1),
-               dateRangeInput("dateRange", "Date range",
+               dateRangeInput("dateRange_county", "Date range",
                               start  = min(data.county$date),
                               end    = max(data.county$date), #max(data.confirmed$date)
                               min    = min(data.county$date),
@@ -112,14 +149,40 @@ shinyUI(
                ),
              mainPanel(
                h3(textOutput("caption_county")),
-               textOutput("startdate_county"),
-               plotOutput("countyPlot"))
+               plotOutput("englandcountyPlot"),
+               h6("Made by Max Eyre"),
+               h6("Any comments, questions or suggestions please contact via twitter or max.eyre@lstmed.ac.uk"),
+               uiOutput("twitter3"),
+               uiOutput("data_source3")
+               )
+           )
+           ),
+  tabPanel("UK Testing",
+           headerPanel("COVID-19 Data Visualisation"),   
+           h3("Live diagnostic testing rates for UK"),
+           h6("Data source: Public Health England (updated every 24hrs)"),
+           sidebarLayout(
+             sidebarPanel(
+               checkboxGroupInput("checkGroup_test", "", choices = list("Tested (daily)" = "new_tested", 
+                                                                          "Tested (total)" = "total_tested"),selected = 1),
+               dateRangeInput("dateRange_test", "Date range",
+                              start  = min(data.test$date),
+                              end    = max(data.test$date), 
+                              min    = min(data.test$date),
+                              max    = max(data.test$date)),
+               radioButtons("log_test", "y-axis scale:",
+                            choices=c('Linear'="log_no",
+                                      'Log'='log_yes'))
+             ),
+             mainPanel(plotOutput("UKtestingPlot"),
+                       h6("Made by Max Eyre"),
+                       h6("Any comments, questions or suggestions please contact via twitter or max.eyre@lstmed.ac.uk"),
+                       uiOutput("twitter4"),
+                       uiOutput("data_source4")
+                       )
            )
            )
   )
-)
+  )
 
-#h6("Made by Max Eyre"),
-#h6("Any comments, questions or suggestions please contact via twitter or max.eyre@lstmed.ac.uk"),
-#uiOutput("twitter"),
-#uiOutput("data_source")
+
