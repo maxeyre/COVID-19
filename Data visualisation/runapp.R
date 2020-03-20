081caa5a2de30f9a73a95007c6859a8750d6ead2
Max <maxeyre3@gmail.com>
2020-03-20 10:09:13 +0000

    push

--- Data visualisation/runapp.R
+++ Data visualisation/runapp.R
@@ -2,8 +2,8 @@ library(shiny)
 runApp("shinyapp/")
 
 rsconnect::setAccountInfo(name='maxeyre',
-                          token='4F867E67A24716CACF3E0D31D9395046',
-                          secret='kJmQRMRN/HyEx/tkwlgULIsrLi4mmng8wCRCem5v')
+                          token='SECRET',
+                          secret='SECRET')
 
 
 rsconnect::deployApp('shinyapp/')
