e7e8ed370fee74f827c7b0ce3f42ceb76387b9b6
Max <maxeyre3@gmail.com>
2020-04-07 19:02:55 +0100

    update

--- data_scraper/01_download-daily-PHE_data.R
+++ data_scraper/01_download-daily-PHE_data.R
@@ -11,33 +11,48 @@ url <- "https://fingertips.phe.org.uk/documents/Historic%20COVID-19%20Dashboard%
 # Download file and load it in R
 download.file(url, destfile = "data/original/PHE_main_data.xlsx")
 
-# UK total
-UK_total.n <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
+# LOAD FROM PHE
+UK_total <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
                            sheet = 2, 
                            skip = 8)
-UK_countries_deaths.n <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
+UK_countries_deaths <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
                                       sheet = 3, 
                                       skip = 7)
 
-UK_countries_cases.n <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
+UK_countries_cases <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
                                          sheet = 4, 
                                          skip = 7)
 
-england_region.n <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
+england_region <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
                                     sheet = 5, 
                                     skip = 7)
 
-england_countyUA.n <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
+england_countyUA <- readxl::read_xlsx(path = "data/original/PHE_main_data.xlsx", 
                                     sheet = 6, 
                                     skip = 7)
-
-
+UK_total_original <- read.csv("data/original/UK_total.csv")
+UK_total_original$date <- as.Date(UK_total_original$date, "%d/%m/%Y")
 
 # DATA PROCESSING --------------------------------------------------------------
 
 # UK_total
+UK_total <- UK_total %>%
+  rename(date = Date, total_cases = `Cumulative Cases`)
+dat <- data.frame(date=UK_total$date[UK_total$date>"2020-03-09"], total_cases = UK_total$total_cases[UK_total$date>"2020-03-09"],
+                  total_deaths=UK_countries_deaths$UK[UK_countries_deaths$Date>"2020-03-09"])
 
+UK_total <- rbind(UK_total_original,dat)
 
+# UK_countries_deaths
+UK_countries_deaths <- UK_countries_deaths %>%
+  select(-Deaths)
+
+# UK_countries_cases
+UK_countries_cases <- UK_countries_cases %>%
+  select(-`Area Code`, country = `Area Name`) %>%
+  filter(country!= "UK") %>%
+  tidyr::gather(key="date", value="cum_cases", -country) %>%
+  mutate(date=as.Date(as.numeric(date), origin="1899-12-30"))
  
 # # Convert to long format and 
 # cases_long <- cases %>% 
