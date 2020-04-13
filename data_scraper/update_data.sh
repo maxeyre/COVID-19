8e94b82cdb48f9df29a23cea8d673ab9addd4fc5
Max <maxeyre3@gmail.com>
2020-04-13 17:38:12 +0100

    “scrape”

--- data_scraper/update_data.sh
+++ data_scraper/update_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-cd Documents/4_Projects/COVID-19/data_scraper/
+cd Documents/4_Projects/COVID-19/data_scraper
 Rscript 01_download-daily-PHE_data.R
 Rscript 02_download-daily-JHU_data.R
 Rscript 03_download-daily-brazil_data.R
