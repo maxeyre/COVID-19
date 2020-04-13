f5af63be70942436fc170ba001e2c86edc2af85d
Max <maxeyre3@gmail.com>
2020-04-13 17:10:13 +0100

    “scrape”

--- data_scraper/update_data.sh
+++ data_scraper/update_data.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-cd Documents/4_Projects/COVID-19/data_scraper
+cd Documents/4_Projects/COVID-19/data_scraper/
 Rscript 01_download-daily-PHE_data.R
 Rscript 02_download-daily-JHU_data.R
 Rscript 03_download-daily-brazil_data.R
 git add -A
 git commit -m “scrape”
-git push
+git push origin master
 echo “Data scraped”
